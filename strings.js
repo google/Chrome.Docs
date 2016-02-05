4673a94d0558fdba01e0efae77211f0ec868f0d6
Joe Medley <jmedley@google.com>
2016-02-05 15:04:53 -0800

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,5 +1,5 @@
 var AVAILABLE = "# %s\n\nIssue: [%s](%s)\n\n[MDN documentation is already available]() for this feature. An appropriate Chrome version numnber will be added when this featuere reaches a stable release.";
-var CODEPATH = "https://bugs.chromium.org/p/v8/issues/detail?id="
+var CODEPATH = "https://bugs.chromium.org/p/v8/issues/detail?id=%s"
 var CR_CODEPATH = "https://code.google.com/p/chromium/issues/detail?id=%s";
 var DEFUNCT_MSG = 'This page is deprecated. It\'s contents have been moved to [Mozilla Developer Network](https://developer.mozilla.org/en-US/)\n\n';
 
