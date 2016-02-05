64d249b0918099783e1f63b1d908221fec0d48c4
jmedley@google.com <jmedley@google.com>
2016-02-05 14:13:22 -0800

    Add a page generator to the gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var argv = require('yargs').argv;
 var fs = require('fs');
 var gulp = require('gulp');
 var path = require('path');
+var strings = require('./strings.js');
+var util = require('util');
 
 var DEFUNCT_MSG = 'This page is deprecated. It\'s contents have been moved to [Mozilla Developer Network](https://developer.mozilla.org/en-US/)\n\n';
 
@@ -23,4 +25,18 @@ gulp.task('stamp', function() {
     contents = DEFUNCT_MSG + contents;
     fs.writeFileSync(filepath, contents, "utf8");
   }
+});
+
+gulp.task('makepage', function() {
+  var title = String(argv.title);
+  var outPath = String(argv.atPath);
+  var issue = String(argv.issue);
+  var issuePath = util.format(strings.codepath, issue);
+  var fileName = (argv.fileName ? String(argv.fileName) : title )
+  fileName = ((fileName.indexOf(".md")==-1) ? (fileName + ".md") : fileName)
+  outPath = path.join(outPath, fileName);
+
+
+  var contents = util.format(strings.available, title, issue, issuePath)
+  fs.writeFileSync(outPath, contents, "utf8")
 });
---
+++ strings.js
@@ -0,0 +1,7 @@
+var AVAILABLE = "# %s\n\nIssue: [%s](%s)\n\n[MDN documentation is already available]() for this feature. An appropriate Chrome version numnber will be added when this featuere reaches a stable release.";
+var CODEPATH = "https://code.google.com/p/chromium/issues/detail?id=%s";
+
+module.exports = {
+	available: AVAILABLE,
+	codepath: CODEPATH
+};
